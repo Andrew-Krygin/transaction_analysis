a3246804c7f857b902e8a4501c24698291babac3
Andrew Krygin <andrew_krygin@protonmail.com>
2025-06-25 00:18:38 +0300

    Add utils module with core helper functions:
    load_operations for reading and preprocessing transactions from Excel,
    
    get_month_range to calculate date ranges for a month,
    
    get_user_settings to load user preferences from JSON,
    all with detailed logging and error handling.

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,86 @@
+import json
+import os
+from datetime import datetime
 
+import pandas as pd
+
+from src.settings_logger import setup_logger
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PATH = os.path.join(BASE_DIR, "data", "operations.xlsx")
+
+logger = setup_logger(__name__, "DEBUG", "views.log")
+
+
+# Функция загружает данные из файла operations.xlsx
+def load_operations(file_path: str) -> pd.DataFrame:
+    """
+    Функция загружает операции из Excel-файла.
+    :param file_path: Путь к файлу.
+    :return: Dataframe с транзакциями.
+    """
+    logger.info("The function load_operations started. File path: %s", file_path)
+    try:
+        logger.debug("Loading Dataframe.")
+        df = pd.read_excel(file_path)
+
+        logger.debug("Handling empty or nan values in Dataframe.")
+        df = df.fillna("")
+
+        logger.debug("Converting 'Дата операции' column to datetime objects.")
+        df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
+
+        logger.info("The function completed successfully! Return DateFrame shape (rows, columns): %s", df.shape)
+        return df
+    except Exception:
+        logger.error("Unexpected error during load_operations execution")
+        raise
+
+
+# Функция возвращает диапазон от начала месяца до указанной даты.
+def get_month_range(date: str) -> tuple[datetime, datetime]:
+    """
+    Функция возвращает диапазон от начала месяца до указанной даты.
+    :param date: Дата.
+    :return: Возвращает кортеж дат, от начала месяца до указанной даты.
+    """
+    logger.info("The function get_month_range started. Date: %s", date)
+
+    logger.debug("Convert a date from type 'str' to a datetime object.")
+    dt_date = datetime.strptime(date, "%d.%m.%Y %H:%M:%S")
+
+    logger.debug("Replacing day, hour, minute, second and microsecond to get the start of the month.")
+    start_date = dt_date.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+
+    logger.info("The function completed successfully! Return start_date - %s, date - %s", start_date, dt_date)
+    return start_date, dt_date
+
+
+# Функция для чтения настроек из файла user_settings.json
+def get_user_settings() -> tuple[list, list]:
+    """
+    Функция читает настройки из файла user_settings.json.
+    :return: Кортеж с данными.
+    """
+    logger.info("The function get_user_settings started.")
+    try:
+        logger.debug("The user_settings.json file is being read.")
+        with open(f"{BASE_DIR}/data/user_settings.json", encoding="utf-8") as f:
+            data = json.load(f)
+
+            logger.info(
+                "The function get_user_settings read the file successfully. Return user_currencies: %s, "
+                "user_stocks: %s",
+                data["user_currencies"],
+                data["user_stocks"],
+            )
+            return data["user_currencies"], data["user_stocks"]
+    except FileNotFoundError:
+        logger.error("Error: Not such file.")
+        raise
+    except json.JSONDecodeError as json_err:
+        logger.error("JSON Decode Error: %s", json_err)
+        raise
+    except Exception:
+        logger.error("Unexpected error during get_user_settings execution.")
+        raise
