cb7d72cac27d87c09d72444719139447c4fe63cb
Andrew Krygin <andrew_krygin@protonmail.com>
2025-07-05 22:55:01 +0300

    Update conftest module: Add three fixtures for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,3 +18,29 @@ def dataframe_transactions() -> pd.DataFrame:
             "Сумма": [100, 200, 300, 400, 7543],
         }
     )
+
+
+@pytest.fixture
+def sample_choice_menu() -> dict:
+    return {
+        "Главная": {
+            1: "1 категория",
+            2: "1 категория",
+            3: "1 категория",
+            4: "1 категория",
+        },
+        "Сервисы": {1: "1 категория"},
+        "Отчеты": {1: "1 категория"},
+    }
+
+
+@pytest.fixture
+def sample_df_1() -> pd.DataFrame:
+    data = {"Сумма платежа": [-100, 200, -50], "Категория": ["Еда", "Зарплата", "Транспорт"]}
+    return pd.DataFrame(data)
+
+
+@pytest.fixture
+def sample_df_2() -> pd.DataFrame:
+    data = {"Дата операции": pd.to_datetime(["2025-06-01", "2025-06-15", "2025-07-01"]), "Сумма": [-100, -50, 200]}
+    return pd.DataFrame(data)
